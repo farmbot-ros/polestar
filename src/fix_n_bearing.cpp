2881ddc37eefd0efdde121897d829b0be8571da0
Trim Bresilla <trim.bresilla@gmail.com>
2024-11-10 23:01:13 +0100

    refactor: rename `angle_deg` to `heading` throughout the code
    - Change the parameter from `angle_deg` to `heading` in the YAML configuration.
    - Modify the `toRadians` function to return a negative value and ensure it uses modulo operation.
    - Update the class variable from `angle_deg` to `heading`.
    - Adjust the subscriptions and parameters to reflect the change from `angle_deg` to `heading`.
    - Update logging messages to use `heading` instead of `angle_deg`.

--- src/fix_n_bearing.cpp
+++ src/fix_n_bearing.cpp
@@ -10,22 +10,23 @@
 #include "message_filters/sync_policies/approximate_time.h"
 
 double toRadians(double degrees) {
-    return degrees * M_PI / 180.0;
+    auto rads = std::fmod(degrees * M_PI / 180.0, 360.0);
+    return -rads;
 }
 
 class GpsAndDEg : public rclcpp::Node {
     private:
         sensor_msgs::msg::NavSatFix curr_gps;
-        std_msgs::msg::Float32 angle_deg;
+        std_msgs::msg::Float32 heading;
         std::string gps_corr_topic;
-        std::string angle_deg_topic;
+        std::string heading_topic;
 
         std::string name;
         std::string frame_id;
 
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_corr_;
-        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr angle_deg_;
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr head_pub_;
         rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr gps_pub_;
         rclcpp::Publisher<farmbot_interfaces::msg::Float32Stamped>::SharedPtr deg_;
         rclcpp::Publisher<farmbot_interfaces::msg::Float32Stamped>::SharedPtr rad_;
@@ -44,21 +45,21 @@ class GpsAndDEg : public rclcpp::Node {
                 name = "fix_n_bearing";
             }
 
-            //try to get the parameters of gps_corr and angle_deg topics
+            //try to get the parameters of gps_corr and heading topics
             try{
                 rclcpp::Parameter gps_corr_param = this->get_parameter("gps_corr");
                 gps_corr_topic = gps_corr_param.as_string();
-                rclcpp::Parameter angle_gpses_param = this->get_parameter("angle_deg");
-                angle_deg_topic = angle_gpses_param.as_string();
+                rclcpp::Parameter angle_gpses_param = this->get_parameter("heading");
+                heading_topic = angle_gpses_param.as_string();
             } catch(const std::exception& e) {
-                RCLCPP_WARN(this->get_logger(), "Could not find one of those parameters: gps_corr, angle_deg");
+                RCLCPP_WARN(this->get_logger(), "Could not find one of those parameters: gps_corr, heading");
                 gps_corr_topic = "gps_corr";
-                angle_deg_topic = "angle_deg";
+                heading_topic = "heading";
             }
-            RCLCPP_INFO(this->get_logger(), "Subscribing to %s and %s", gps_corr_topic.c_str(), angle_deg_topic.c_str());
+            RCLCPP_INFO(this->get_logger(), "Subscribing to %s and %s", gps_corr_topic.c_str(), heading_topic.c_str());
 
             gps_corr_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(gps_corr_topic, 10, std::bind(&GpsAndDEg::gps_corr_callback, this, std::placeholders::_1));
-            angle_deg_ = this->create_subscription<std_msgs::msg::Float32>(angle_deg_topic, 10, std::bind(&GpsAndDEg::angle_deg_callback, this, std::placeholders::_1));
+            head_pub_ = this->create_subscription<std_msgs::msg::Float32>(heading_topic, 10, std::bind(&GpsAndDEg::angle_deg_callback, this, std::placeholders::_1));
             timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&GpsAndDEg::timer_callback, this));
             gps_pub_ = this->create_publisher<sensor_msgs::msg::NavSatFix>("loc/fix", 10);
             deg_ = this->create_publisher<farmbot_interfaces::msg::Float32Stamped>("loc/deg", 10);
@@ -76,7 +77,7 @@ class GpsAndDEg : public rclcpp::Node {
         }
 
         void angle_deg_callback(const std_msgs::msg::Float32::ConstSharedPtr& angle_deg_msg) {
-            angle_deg = *angle_deg_msg;
+            heading = *angle_deg_msg;
         }
 
         void timer_callback() {
@@ -87,11 +88,11 @@ class GpsAndDEg : public rclcpp::Node {
             gps_pub_->publish(curr_pose);
             farmbot_interfaces::msg::Float32Stamped deg_msg;
             deg_msg.header = curr_pose.header;
-            deg_msg.data = angle_deg.data;
+            deg_msg.data = heading.data;
             deg_->publish(deg_msg);
             farmbot_interfaces::msg::Float32Stamped rad_msg;
             rad_msg.header = curr_pose.header;
-            rad_msg.data = toRadians(angle_deg.data);
+            rad_msg.data = toRadians(heading.data);
             rad_->publish(rad_msg);
         }
 };
