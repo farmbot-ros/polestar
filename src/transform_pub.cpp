e710c6d0ca9082415c78f79a776f62f062468313
Trim Bresilla <trim.bresilla@gmail.com>
2024-11-18 19:28:33 +0100

    refactor: enhance transform logic and logging with odometry data
    - Update default `localization_type` to "fix_n_bearing" and `autodatum` to "auto" in `params.yaml`.
    - Add `odom_msg` to store odometry data in the `TransformPub` class.
    - Change `odom_tf` type from `StaticTransformBroadcaster` to `TransformBroadcaster`.
    - Simplify parameter retrieval by using `get_parameter_or` for "name" and "altitude".
    - Remove redundant `namespace_2` variable.
    - Update transform functions to use odometry data for generating transforms.
    - Improve logging with a single constructor info message and categorizing transform setup.

--- src/transform_pub.cpp
+++ src/transform_pub.cpp
@@ -18,9 +18,11 @@ float removeFirstFourDigits(float number, unsigned long digits=5) {
     }
 }
 
+
 class TransformPub : public rclcpp::Node {
     private:
         nav_msgs::msg::Odometry ecef_msg;
+        nav_msgs::msg::Odometry odom_msg;
         bool is_transformed = false;
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
         rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr ecef_sub_;
@@ -29,11 +31,10 @@ class TransformPub : public rclcpp::Node {
         std::string name;
         std::string transform_from_;
         std::string namespace_;
-        std::string namespace_2;
         bool altitude;
 
         std::unique_ptr<tf2_ros::TransformBroadcaster> base_tf;
-        std::unique_ptr<tf2_ros::StaticTransformBroadcaster> odom_tf;
+        std::unique_ptr<tf2_ros::TransformBroadcaster> odom_tf;
         std::unique_ptr<tf2_ros::StaticTransformBroadcaster> map_tf;
 
     public:
@@ -44,85 +45,77 @@ class TransformPub : public rclcpp::Node {
             .automatically_declare_parameters_from_overrides(true)
         ){
             RCLCPP_INFO(this->get_logger(), "Starting TransformPub node");
-
-            try {
-                name = this->get_parameter("name").as_string();
-
-            } catch (...) {
-                name = "transform_pub";
-            }
-
+            name = this->get_parameter_or<std::string>("name", "transform_pub");
             altitude = this->get_parameter_or<bool>("altitude", false);
 
             namespace_ = this->get_namespace();
             if (!namespace_.empty() && namespace_[0] == '/') {
                 namespace_ = namespace_.substr(1); // Remove leading slash
             }
-            namespace_2 = namespace_;
-
 
             odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("loc/odom", 10, std::bind(&TransformPub::base_transform, this, std::placeholders::_1));
             ecef_sub_ = this->create_subscription<nav_msgs::msg::Odometry>("loc/ref", 10, std::bind(&TransformPub::ecef_callback, this, std::placeholders::_1));
             transform_timer_ = this->create_wall_timer(std::chrono::milliseconds(10000), std::bind(&TransformPub::ecef_timer, this));
 
             base_tf = std::make_unique<tf2_ros::TransformBroadcaster>(this);
-            odom_tf = std::make_unique<tf2_ros::StaticTransformBroadcaster>(this);
+            odom_tf = std::make_unique<tf2_ros::TransformBroadcaster>(this);
             map_tf = std::make_unique<tf2_ros::StaticTransformBroadcaster>(this);
-            RCLCPP_INFO(this->get_logger(), "--------------------------------------------------");
-            RCLCPP_INFO(this->get_logger(), "NAMESPACE: %s", namespace_.c_str());
-            RCLCPP_INFO(this->get_logger(), "--------------------------------------------------");
         }
 
     private:
         void ecef_callback(const nav_msgs::msg::Odometry::ConstSharedPtr& odom) {
             ecef_msg = *odom;
             map_transform();
-            odom_transform();
             is_transformed = true;
-            RCLCPP_INFO(this->get_logger(), "TRANSFORMS SET");
+            RCLCPP_INFO(this->get_logger(), "--------------------------------------------------");
+            RCLCPP_INFO_ONCE(this->get_logger(), "TRANSFORMS FOR <%s> SET", namespace_.c_str());
             ecef_sub_.reset();
-            RCLCPP_INFO(this->get_logger(), "SUBSCRIPTION CLOSED");
         }
 
         void ecef_timer() {
             if (is_transformed) {
                 map_transform();
-                odom_transform();
                 return;
             }
             // RCLCPP_INFO(this->get_logger(), "WAITING FOR TRANSFORM, PLEASE SET DATUM FIRST!");
         }
 
         void base_transform(const nav_msgs::msg::Odometry::ConstSharedPtr& odom) {
+            odom_msg = *odom;
+
+            // Send identity transform from /odom to /base_link
             geometry_msgs::msg::TransformStamped dyna_t;
             dyna_t.header.stamp = odom->header.stamp;
             dyna_t.header.frame_id = namespace_ + "/odom";
             dyna_t.child_frame_id = namespace_ + "/base_link";
-            dyna_t.transform.translation.x = odom->pose.pose.position.x;
-            dyna_t.transform.translation.y = odom->pose.pose.position.y;
-            dyna_t.transform.translation.z = odom->pose.pose.position.z;
-            if (!altitude) {
-                dyna_t.transform.translation.z = 0.0;
-            }
-            dyna_t.transform.rotation.x = odom->pose.pose.orientation.x;
-            dyna_t.transform.rotation.y = odom->pose.pose.orientation.y;
-            dyna_t.transform.rotation.z = odom->pose.pose.orientation.z;
-            dyna_t.transform.rotation.w = odom->pose.pose.orientation.w;
+            dyna_t.transform.translation.x = 0.0;
+            dyna_t.transform.translation.y = 0.0;
+            dyna_t.transform.translation.z = 0.0;
+            dyna_t.transform.rotation.x = 0.0;
+            dyna_t.transform.rotation.y = 0.0;
+            dyna_t.transform.rotation.z = 0.0;
+            dyna_t.transform.rotation.w = 1.0;
             base_tf->sendTransform(dyna_t);
+
+            // Send odometry transform from /map to /odom
+            odom_transform();
         }
 
         void odom_transform() {
             geometry_msgs::msg::TransformStamped stat_t;
-            stat_t.header.stamp = this->get_clock()->now();
-            stat_t.header.frame_id = namespace_2 + "/map";
+            stat_t.header.stamp = odom_msg.header.stamp;
+            stat_t.header.frame_id = namespace_ + "/map";
             stat_t.child_frame_id = namespace_ + "/odom";
-            stat_t.transform.translation.x = 0.0;
-            stat_t.transform.translation.y = 0.0;
-            stat_t.transform.translation.z = 0.0;
-            stat_t.transform.rotation.x = 0.0;
-            stat_t.transform.rotation.y = 0.0;
-            stat_t.transform.rotation.z = 0.0;
-            stat_t.transform.rotation.w = 1.0;
+            stat_t.transform.translation.x = odom_msg.pose.pose.position.x;
+            stat_t.transform.translation.y = odom_msg.pose.pose.position.y;
+            stat_t.transform.translation.z = odom_msg.pose.pose.position.z;
+            if (!altitude) {
+                stat_t.transform.translation.z = 0.0;
+            }
+            stat_t.transform.rotation.x = odom_msg.pose.pose.orientation.x;
+            stat_t.transform.rotation.y = odom_msg.pose.pose.orientation.y;
+            stat_t.transform.rotation.z = odom_msg.pose.pose.orientation.z;
+            stat_t.transform.rotation.w = odom_msg.pose.pose.orientation.w;
             odom_tf->sendTransform(stat_t);
         }
 
@@ -133,8 +126,8 @@ class TransformPub : public rclcpp::Node {
             // RCLCPP_INFO(this->get_logger(), "ECEF: %f, %f, %f", x, y, z);
             geometry_msgs::msg::TransformStamped stat_t;
             stat_t.header.stamp = this->get_clock()->now();
-            stat_t.header.frame_id = "/world";
-            stat_t.child_frame_id = namespace_2 + "/map";
+            stat_t.header.frame_id = "world";
+            stat_t.child_frame_id = namespace_ + "/map";
             stat_t.transform.translation.x = x;
             stat_t.transform.translation.y = y;
             stat_t.transform.translation.z = z;
@@ -148,8 +141,8 @@ class TransformPub : public rclcpp::Node {
             map_tf->sendTransform(stat_t);
         }
 
-
 };
+
 
 int main(int argc, char *argv[]) {
     rclcpp::init(argc, argv);
