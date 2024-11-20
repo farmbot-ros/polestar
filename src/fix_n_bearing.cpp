58aa66809b0452cf7fac869cd4b4689bf889731f
Trim Bresilla <trim.bresilla@gmail.com>
2024-11-20 02:59:18 +0100

    feat: integrate compass support for enhanced navigation
    - Add `compass` and `bearing` parameters to the configuration file.
    - Introduce a function to convert radians to degrees.
    - Add `compass` as a new member variable and corresponding subscription in the `GpsAndDEg` class.
    - Add logic to handle `compass` data in the `timer_callback`, allowing for dynamic selection between `compass` and `heading` for bearings.
    - Change `head_pub_` to `head_sub_` to correct the naming of the heading subscription.

--- src/fix_n_bearing.cpp
+++ src/fix_n_bearing.cpp
@@ -14,19 +14,29 @@ double toRadians(double degrees) {
     return rads;
 }
 
+double toDegrees(double radians) {
+    auto degs = std::fmod(radians * 180.0 / M_PI, 360.0);
+    return degs;
+}
+
 class GpsAndDEg : public rclcpp::Node {
     private:
         sensor_msgs::msg::NavSatFix curr_gps;
         std_msgs::msg::Float32 heading;
+        std_msgs::msg::Float32 compass;
+
         std::string gps_corr_topic;
         std::string heading_topic;
+        std::string compass_topic;
+        std::string bearing;
 
         std::string name;
         std::string frame_id;
 
         rclcpp::TimerBase::SharedPtr timer_;
         rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_corr_;
-        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr head_pub_;
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr head_sub_;
+        rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr comp_sub_;
         rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr gps_pub_;
         rclcpp::Publisher<farmbot_interfaces::msg::Float32Stamped>::SharedPtr deg_;
         rclcpp::Publisher<farmbot_interfaces::msg::Float32Stamped>::SharedPtr rad_;
@@ -51,15 +61,21 @@ class GpsAndDEg : public rclcpp::Node {
                 gps_corr_topic = gps_corr_param.as_string();
                 rclcpp::Parameter angle_gpses_param = this->get_parameter("heading");
                 heading_topic = angle_gpses_param.as_string();
+                rclcpp::Parameter compass_param = this->get_parameter("compass");
+                compass_topic = compass_param.as_string();
+                rclcpp::Parameter bearing_param = this->get_parameter("bearing");
+                bearing = bearing_param.as_string();
             } catch(const std::exception& e) {
                 RCLCPP_WARN(this->get_logger(), "Could not find one of those parameters: gnss/fix, gnss/heading");
                 gps_corr_topic = "gnss/fix";
                 heading_topic = "gnss/heading";
+                compass_topic = "compass";
             }
             RCLCPP_INFO(this->get_logger(), "Subscribing to %s and %s", gps_corr_topic.c_str(), heading_topic.c_str());
 
             gps_corr_ = this->create_subscription<sensor_msgs::msg::NavSatFix>(gps_corr_topic, 10, std::bind(&GpsAndDEg::gps_corr_callback, this, std::placeholders::_1));
-            head_pub_ = this->create_subscription<std_msgs::msg::Float32>(heading_topic, 10, std::bind(&GpsAndDEg::angle_deg_callback, this, std::placeholders::_1));
+            head_sub_ = this->create_subscription<std_msgs::msg::Float32>(heading_topic, 10, std::bind(&GpsAndDEg::angle_deg_callback, this, std::placeholders::_1));
+            comp_sub_ = this->create_subscription<std_msgs::msg::Float32>(compass_topic, 10, std::bind(&GpsAndDEg::angle_rad_callback, this, std::placeholders::_1));
             timer_ = this->create_wall_timer(std::chrono::milliseconds(10), std::bind(&GpsAndDEg::timer_callback, this));
             gps_pub_ = this->create_publisher<sensor_msgs::msg::NavSatFix>("loc/fix", 10);
             deg_ = this->create_publisher<farmbot_interfaces::msg::Float32Stamped>("loc/deg", 10);
@@ -83,19 +99,30 @@ class GpsAndDEg : public rclcpp::Node {
             heading = *angle_deg_msg;
         }
 
+        void angle_rad_callback(const std_msgs::msg::Float32::ConstSharedPtr& angle_rad_msg) {
+            compass = *angle_rad_msg;
+        }
+
         void timer_callback() {
             sensor_msgs::msg::NavSatFix curr_pose;
             curr_pose = curr_gps;
             curr_pose.header.frame_id = frame_id;
             curr_pose.header.stamp = this->now();
             gps_pub_->publish(curr_pose);
+
             farmbot_interfaces::msg::Float32Stamped deg_msg;
             deg_msg.header = curr_pose.header;
-            deg_msg.data = heading.data;
-            deg_->publish(deg_msg);
             farmbot_interfaces::msg::Float32Stamped rad_msg;
             rad_msg.header = curr_pose.header;
-            rad_msg.data = toRadians(heading.data);
+
+            if (bearing == "compass") {
+                rad_msg.data = compass.data;
+                deg_msg.data = toDegrees(compass.data);
+            } else if (bearing == "heading") {
+                rad_msg.data = toRadians(heading.data);
+                deg_msg.data = heading.data;
+            }
+            deg_->publish(deg_msg);
             rad_->publish(rad_msg);
         }
 };
