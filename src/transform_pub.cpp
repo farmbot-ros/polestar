09065e4ae77be356aa0a596558b69e09da05f558
Trim Bresilla <trim.bresilla@gmail.com>
2024-11-21 00:16:13 +0100

    feat: add diagnostic updates for transform status
    - Add diagnostic message and updater dependencies to `CMakeLists.txt`.
    - Include Diagnostic Updater library in `transform_pub.cpp`.
    - Introduce new boolean member variables for transform status tracking.
    - Add `diagnostic_updater::Updater` instance and corresponding timer for periodic diagnostics.
    - Implement `diagnostic_callback` and `check_system` methods for diagnostic updates.
    - Set the hardware ID and add diagnostic tasks related to transformation status.
    - Update transform functions to modify status flags.

--- src/transform_pub.cpp
+++ src/transform_pub.cpp
@@ -5,6 +5,9 @@
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2_ros/static_transform_broadcaster.h"
 #include "geometry_msgs/msg/transform_stamped.hpp"
+#include "diagnostic_updater/diagnostic_updater.hpp"
+
+using namespace std::chrono_literals;
 
 float removeFirstFourDigits(float number, unsigned long digits=5) {
     std::string numberStr = std::to_string(number);
@@ -20,6 +23,11 @@ float removeFirstFourDigits(float number, unsigned long digits=5) {
 
 class TransformPub : public rclcpp::Node {
     private:
+        bool world_to_map;
+        bool map_to_odom;
+        bool odom_to_basef;
+        bool basef_to_basel;
+
         nav_msgs::msg::Odometry ecef_msg;
         nav_msgs::msg::Odometry odom_msg;
         nav_msgs::msg::Odometry sens_msg;
@@ -37,13 +45,17 @@ class TransformPub : public rclcpp::Node {
         std::unique_ptr<tf2_ros::StaticTransformBroadcaster> odom_tf;
         std::unique_ptr<tf2_ros::StaticTransformBroadcaster> map_tf;
 
+        //Diagnostic Updater
+        diagnostic_updater::Updater updater_;
+        rclcpp::TimerBase::SharedPtr diagnostic_timer_;
+
     public:
         TransformPub() : Node(
             "transform_pub",
             rclcpp::NodeOptions()
             .allow_undeclared_parameters(true)
             .automatically_declare_parameters_from_overrides(true)
-        ){
+        ), updater_(this) {
             RCLCPP_INFO(this->get_logger(), "Starting TransformPub node");
 
             world_ref_ = this->get_parameter_or<std::string>("world_ref", "datum");
@@ -65,12 +77,25 @@ class TransformPub : public rclcpp::Node {
             odom_tf = std::make_unique<tf2_ros::StaticTransformBroadcaster>(this);
             map_tf = std::make_unique<tf2_ros::StaticTransformBroadcaster>(this);
 
-            RCLCPP_INFO(this->get_logger(), "--------------------------------------------------");
-            RCLCPP_INFO(this->get_logger(), "NAMESPACE: %s", namespace_.c_str());
-            RCLCPP_INFO(this->get_logger(), "--------------------------------------------------");
+            // Diagnostic Updater
+            updater_.setHardwareID(static_cast<std::string>(this->get_namespace()) + "/loc");
+            updater_.add("Transformation Status", this, &TransformPub::check_system);
+            diagnostic_timer_ = this->create_wall_timer(1s, std::bind(&TransformPub::diagnostic_callback, this));
         }
 
     private:
+
+        void diagnostic_callback() {
+            updater_.force_update();
+        }
+
+        void check_system(diagnostic_updater::DiagnosticStatusWrapper &stat) {
+            stat.summary(diagnostic_msgs::msg::DiagnosticStatus::OK, "System is running");
+            stat.add("World to Map", world_to_map);
+            stat.add("Map to Odom", map_to_odom);
+            stat.add("Odom to Base Footprint", odom_to_basef);
+            stat.add("Base Footprint to Base Link", basef_to_basel);
+        }
 
         void footprint_transform(const nav_msgs::msg::Odometry::ConstSharedPtr& odom) {
             odom_msg = *odom;
@@ -86,6 +111,7 @@ class TransformPub : public rclcpp::Node {
             dyna_t.transform.rotation.z = odom_msg.pose.pose.orientation.z;
             dyna_t.transform.rotation.w = odom_msg.pose.pose.orientation.w;
             base_tf->sendTransform(dyna_t);
+            basef_to_basel = true;
             baselink_transform(odom);
             odom_transform(odom);
         }
@@ -106,6 +132,7 @@ class TransformPub : public rclcpp::Node {
             foot_t.transform.rotation.z = 0.0;
             foot_t.transform.rotation.w = 1.0;
             foot_tf->sendTransform(foot_t);
+            odom_to_basef = true;
         }
 
         void odom_transform(const nav_msgs::msg::Odometry::ConstSharedPtr& sens) {
@@ -122,6 +149,7 @@ class TransformPub : public rclcpp::Node {
             stat_t.transform.rotation.z = 0.0;
             stat_t.transform.rotation.w = 1.0;
             odom_tf->sendTransform(stat_t);
+            map_to_odom = true;
         }
 
         // void map_transform() {
@@ -141,6 +169,7 @@ class TransformPub : public rclcpp::Node {
             stat_t.transform.rotation.z = 0.0;
             stat_t.transform.rotation.w = 1.0;
             map_tf->sendTransform(stat_t);
+            world_to_map = true;
             ecef_sub_.reset();
         }
 
